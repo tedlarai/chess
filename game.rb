5d0b58e51a2e05b6c45d5832d3562f7025b57d12
tedlarai <thadeularanja@gmail.com>
2015-07-22 12:39:38 -0300

    modified player, started Pawn<Piece

---
+++ game.rb
@@ -0,0 +1,7 @@
+class Game
+
+  def initialize
+    @white = Player.new("white")
+    @black = Player.new("black")
+
+end
--- player.rb
+++ player.rb
@@ -1,11 +1,11 @@
 class Player
   attr_reader(:color)
   def initialize(color)
-    @color = color.capitalize
+    @color = color
   end
 
   def prompt_move #private
-    puts "#{@color}, your move: (ex.: a1 c4)"
+    puts "#{@color.capitalize}, your move: (ex.: a1 c4)"
     move = gets.chomp.downcase
   end
 
@@ -17,13 +17,22 @@ class Player
       end
   end
 
+  def format_move(input_move)
+    output = input_move.split("")-[" "] #creates an array [from_row, from_col, to_row, to_col]
+    output_move = {from_row: map_row(output[0]), from_col: output[1], to_row: map_row(output[2]), to_col: output[3]}
+  end
+
+  def map_row(row)
+    letters = Array("a".."z")
+    letters.find_index(row)+1
+  end
+
   def move
     input_move = nil
     loop do
       input_move = check_move(prompt_move)
       break unless input_move.nil?
     end
-    output = input_move.split(" ")
-    output_move = {from: output[0], to: output[1]}
+    output_move = format_move(input_move)
   end
 end
